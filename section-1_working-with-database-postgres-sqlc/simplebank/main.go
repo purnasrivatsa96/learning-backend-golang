abcadcc2bc2ef98c87945322420f64dd609aec92
Purna Srivatsa <purnasrivatsa96@gmail.com>
2023-04-11 20:36:19 -0400

    section 2: 11. Implement RESTful...

---
+++ section-1_working-with-database-postgres-sqlc/simplebank/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+
+	"github.com/purnasrivatsa96/learning-backend-golang/tree/main/section-1_working-with-database-postgres-sqlc/simplebank/api"
+	db "github.com/purnasrivatsa96/learning-backend-golang/tree/main/section-1_working-with-database-postgres-sqlc/simplebank/db/sqlc"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	dbDriver      = "postgres"
+	dbSource      = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
+	serverAddress = "0.0.0.0:8080"
+)
+
+func main() {
+	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("cannot connect to db: ", err)
+	}
+
+	store := db.NewStore(conn)
+
+	server := api.NewServer(store)
+
+	err = server.Start(serverAddress)
+	if err != nil {
+		log.Fatal("cannot start server: ", err)
+	}
+}
